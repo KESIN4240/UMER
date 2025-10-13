17b4a8c7aa868c644de4430223b519e6c17b6ddc
KESIN4240 <oleksandrkesin@gmail.com>
2025-10-13 18:52:06 +0300

    add visual ant audio efeckts

--- Assets/Scripts/PlayerControlier.cs
+++ Assets/Scripts/PlayerControlier.cs
@@ -5,23 +5,35 @@ public class PlayerControlier : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isGameOver;
+    public AudioClip jumpclip;
+    public AudioClip crashClip;
+    public ParticleSystem crashParticle;
+    public ParticleSystem dirtParticle;
 
     private Rigidbody rigidbody;
+    private Animator animator;
+    private AudioSource audioSource;
     private bool isOnGround = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
         Physics.gravity = Physics.gravity * gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true && isGameOver == false)
         {
             rigidbody.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
             isOnGround = false;
+            animator.SetTrigger("Jump_trig");
+            audioSource.PlayOneShot(jumpclip);
+            dirtParticle.Stop();
         }
     }
 
@@ -31,9 +43,15 @@ public class PlayerControlier : MonoBehaviour
         {
             isGameOver = true;
 
+            animator.SetBool("Death_b", true);
+            animator.SetInteger("DeathType_int", 1);
+            audioSource.PlayOneShot(crashClip);
+            crashParticle.Play();
+            dirtParticle.Stop();
             return;
         }
 
         isOnGround = true;
+        dirtParticle.Play();
     }
 }
