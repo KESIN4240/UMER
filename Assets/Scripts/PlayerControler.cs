aea63787cdcee2686e61d7d8e9e619b98d395a68
KESIN4240 <oleksandrkesin@gmail.com>
2025-12-08 18:36:17 +0200

    Player movement

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
+    public float moveSpeed;
+    public Transform focalPoint;
+
+    private Rigidbody rigidbody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float verticalInput = Input.GetAxis("Vertical");
+        rigidbody.AddForce(focalPoint.forward * moveSpeed * verticalInput);
     }
 }
